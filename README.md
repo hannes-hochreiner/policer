0b52bf25d463d3240cc613f89d9df5f5732fcc93
Hannes Hochreiner <hannes@hochreiner.net>
2022-10-29 21:10:16 +0200

    add police function

--- README.md
+++ README.md
@@ -2,6 +2,35 @@
 
 A utility function to apply a policy to a list of dates.
 
+## Context
+
+The library was written as a utility for applying backup policies.
+The idea is that backups (snapshots) would be made continuously, but not all snapshots should or can be kept.
+Hence, a policy is drawn up, stating which snapshots should be kept.
+For example, backups are made every 30 minutes.
+One snapshot should be 30 minutes or more recent.
+One snapshot should be 1 day or more recent.
+One snapshot should be 1 month or more recent.
+The final snapshot should be the most recent snapshot older than one month.
+The policy in this case would be the intervals 30 minutes, 1 day, and 1 month.
+The police function returns the snapshots not complying with the policy.
+
+## Functions
+
+### police
+
+The police function takes the current date and a list of intervals from this date (i.e., the policy).
+It then compares a list of dates (actually tuples of dates and objects) to the policy intervals and applies the following logic:
+* every space between two intervals is interpreted as a bucket
+* the space before the first and after the last interval is also counted as a bucket (i.e., the number of buckets will be equal to the number of intervals plus one)
+* for every bucket the best fitting date is chosen
+* if there are several dates in a bucket
+  * only the oldest date is kept, if the bucket is not the last bucket (i.e., the one furthers in the past)
+  * if the bucket is the oldest, the newest date is kept
+* if the number of retained dates is smaller than the number of buckets, dates from the list of dates to be deleted are added until
+  * either there are no more dates to be deleted
+  * or the number of retained dates equals the number of buckets
+
 ## License
 
 This work is licensed under the MIT or Apache 2.0 license.
--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,189 @@
-use chrono::{FixedOffset, DateTime};
+use chrono::{FixedOffset, DateTime, Duration};
 
-pub fn apply_policy<'a, T>(now: DateTime<FixedOffset>, policy: &[&DateTime<FixedOffset>], list: &'a[(DateTime<FixedOffset>, T)]) -> Vec<(&'a DateTime<FixedOffset>, &'a T)> {
-    todo!()
+/// Return a list of dates to be deleted
+///
+/// The policy vector is taken to create buckets, where the entries in the vector are the limits.
+/// Two additional buckets are created for the time before the last entry of the vector and after the first entry.
+/// All backups are assigned to these buckets starting with the nearest past.
+/// If there is no entry found for a bucket, the next newest entry will be used.
+/// If there are multiple entries in one bucket, the oldest entry will be kept.
+/// For the bucket with the oldest entries, only the newest entry will be kept.
+///
+/// * `now` - current date and time
+/// * `policy` - a vector of durations
+/// * `list` - a vector of tuples of dates and objects
+///
+pub fn police<'a, T>(now: &DateTime<FixedOffset>, policy: &[Duration], list: &'a[(DateTime<FixedOffset>, T)]) -> Vec<&'a(DateTime<FixedOffset>, T)> {
+    let mut bucket_vec: Vec<&(DateTime<FixedOffset>, T)> = Vec::new();
+    let mut policy: Vec<&Duration> = policy.iter().collect();
+
+    policy.sort();
+
+    let mut policy_iter = policy.iter();
+    let mut policy_elem = policy_iter.next();
+    let mut res: Vec<&(DateTime<FixedOffset>, T)> = Vec::new();
+    let mut list: Vec<&(DateTime<FixedOffset>, T)> = list.iter().collect();
+
+    list.sort_by_key(|e| e.0);
+
+    for &item in list.iter().rev() {
+        match policy_elem {
+            Some(&p) => {
+                if *now - item.0 > *p {
+                    if !bucket_vec.is_empty() {
+                        bucket_vec.pop();
+                        res.append(&mut bucket_vec);
+                        bucket_vec.push(item);
+                    }
+
+                    policy_elem = policy_iter.next();
+                } else {
+                    bucket_vec.push(item);
+                }
+            }
+            None => {
+                bucket_vec.push(item);
+            }
+        }
+    }
+
+    if !bucket_vec.is_empty() {
+        bucket_vec.remove(0);
+        res.append(&mut bucket_vec);
+    }
+
+    while !res.is_empty() && list.len() - res.len() <= policy.len() {
+        res.remove(0);
+    }
+
+    res
 }
 
 #[cfg(test)]
 mod tests {
-    use chrono::Utc;
+    use chrono::{Utc, TimeZone};
 
     use super::*;
 
     #[test]
-    fn empty_policy() {
+    fn empty_policy_one_element() {
         let list: Vec<(DateTime<FixedOffset>, String)> = vec![(Utc::now().into(), "test".to_string())];
-        let result = apply_policy(Utc::now().into(), &[], &list[..]);
+        let result = police(&Utc::now().into(), &[], &list[..]);
+        assert_eq!(result.len(), 0);
+    }
+
+    #[test]
+    fn empty_policy_two_elements() {
+        let list: Vec<(DateTime<FixedOffset>, String)> = vec![(Utc::now().into(), "test1".to_string()), (Utc::now().into(), "test2".to_string())];
+        let result = police(&Utc::now().into(), &[], &list[..]);
         assert_eq!(result.len(), 1);
+        assert_eq!(result[0].1, "test1");
+    }
+
+    #[test]
+    fn one_policy_two_elements() {
+        let now = Utc.ymd(2022, 10, 29).and_hms(0, 0, 0);
+        let policy: Vec<Duration> = vec![Duration::days(1)];
+        let list: Vec<(DateTime<FixedOffset>, String)> = vec![
+            (Utc.ymd(2022, 10, 22).and_hms(0, 0, 0).into(), "test1".to_string()),
+            (Utc.ymd(2022, 10, 21).and_hms(0, 0, 0).into(), "test2".to_string())
+        ];
+        let result = police(&now.into(), &policy, &list[..]);
+
+        assert_eq!(result.len(), 0);
+    }
+
+    #[test]
+    fn one_policy_three_elements() {
+        let now = Utc.ymd(2022, 10, 29).and_hms(0, 0, 0);
+        let policy: Vec<Duration> = vec![Duration::days(1)];
+        let list: Vec<(DateTime<FixedOffset>, String)> = vec![
+            (Utc.ymd(2022, 10, 22).and_hms(0, 0, 0).into(), "test1".to_string()),
+            (Utc.ymd(2022, 10, 21).and_hms(0, 0, 0).into(), "test2".to_string()),
+            (Utc.ymd(2022, 10, 20).and_hms(0, 0, 0).into(), "test3".to_string())
+        ];
+        let result = police(&now.into(), &policy, &list[..]);
+
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].1, "test3");
+    }
+
+    #[test]
+    fn one_policy_four_elements() {
+        let now = Utc.ymd(2022, 10, 29).and_hms(0, 0, 0);
+        let policy: Vec<Duration> = vec![Duration::days(1)];
+        let list: Vec<(DateTime<FixedOffset>, String)> = vec![
+            (Utc.ymd(2022, 10, 22).and_hms(0, 0, 0).into(), "test1".to_string()),
+            (Utc.ymd(2022, 10, 21).and_hms(0, 0, 0).into(), "test2".to_string()),
+            (Utc.ymd(2022, 11, 20).and_hms(0, 0, 0).into(), "test3".to_string()),
+            (Utc.ymd(2022, 12, 20).and_hms(0, 0, 0).into(), "test4".to_string())
+        ];
+        let result = police(&now.into(), &policy, &list[..]);
+
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0].1, "test4");
+        assert_eq!(result[1].1, "test2");
+    }
+
+    #[test]
+    fn two_policies_four_elements_1() {
+        let now = Utc.ymd(2022, 10, 29).and_hms(0, 0, 0);
+        let policy: Vec<Duration> = vec![Duration::days(1), Duration::days(7)];
+        let list: Vec<(DateTime<FixedOffset>, String)> = vec![
+            (Utc.ymd(2022, 10, 28).and_hms(12, 0, 0).into(), "test3".to_string()),
+            (Utc.ymd(2022, 10, 26).and_hms(0, 0, 0).into(), "test4".to_string()),
+            (Utc.ymd(2022, 10, 22).and_hms(0, 0, 0).into(), "test1".to_string()),
+            (Utc.ymd(2022, 10, 21).and_hms(0, 0, 0).into(), "test2".to_string()),
+        ];
+        let result = police(&now.into(), &policy, &list[..]);
+
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].1, "test4");
+    }
+
+    #[test]
+    fn two_policies_four_elements_2() {
+        let now = Utc.ymd(2022, 10, 29).and_hms(0, 0, 0);
+        let policy: Vec<Duration> = vec![Duration::days(1), Duration::days(7)];
+        let list: Vec<(DateTime<FixedOffset>, String)> = vec![
+            (Utc.ymd(2022, 10, 20).and_hms(0, 0, 0).into(), "test1".to_string()),
+            (Utc.ymd(2022, 10, 19).and_hms(0, 0, 0).into(), "test2".to_string()),
+            (Utc.ymd(2022, 10, 28).and_hms(12, 0, 0).into(), "test3".to_string()),
+            (Utc.ymd(2022, 10, 26).and_hms(0, 0, 0).into(), "test4".to_string()),
+        ];
+        let result = police(&now.into(), &policy, &list[..]);
+
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].1, "test2");
+    }
+
+    #[test]
+    fn two_policies_four_elements_3() {
+        let now = Utc.ymd(2022, 10, 29).and_hms(0, 0, 0);
+        let policy: Vec<Duration> = vec![Duration::days(1), Duration::days(7)];
+        let list: Vec<(DateTime<FixedOffset>, String)> = vec![
+            (Utc.ymd(2022, 10, 28).and_hms(13, 0, 0).into(), "test4".to_string()),
+            (Utc.ymd(2022, 10, 28).and_hms(12, 0, 0).into(), "test3".to_string()),
+            (Utc.ymd(2022, 10, 20).and_hms(0, 0, 0).into(), "test1".to_string()),
+            (Utc.ymd(2022, 10, 19).and_hms(0, 0, 0).into(), "test2".to_string()),
+        ];
+        let result = police(&now.into(), &policy, &list[..]);
+
+        assert_eq!(result.len(), 1);
+        assert_eq!(result[0].1, "test4");
+    }
+
+    #[test]
+    fn two_policies_three_elements() {
+        let now = Utc.ymd(2022, 10, 29).and_hms(0, 0, 0);
+        let policy: Vec<Duration> = vec![Duration::days(1), Duration::days(7)];
+        let list: Vec<(DateTime<FixedOffset>, String)> = vec![
+            (Utc.ymd(2022, 10, 28).and_hms(13, 0, 0).into(), "test4".to_string()),
+            (Utc.ymd(2022, 10, 28).and_hms(12, 0, 0).into(), "test3".to_string()),
+            (Utc.ymd(2022, 10, 20).and_hms(0, 0, 0).into(), "test1".to_string()),
+        ];
+        let result = police(&now.into(), &policy, &list[..]);
+
+        assert_eq!(result.len(), 0);
     }
 }
